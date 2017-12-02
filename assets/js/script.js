970969066e97e30f2d2f9c97c0234f554a2a1ea0
Monique Azevedo <moniqueaz.si@gmail.com>
2017-12-02 12:44:09 -0200

    Capitulo 3 completo (seletores de elementos)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,57 @@
 $(document).ready(function(){
-	$('div').css('background', 'lightblue');	
+	//$('td').first().css('background', 'aquamarine');
+ // $('td').last().css('background', 'aquamarine');
+ // $('td')
+ // .first()
+ // .css('background', 'aquamarine')
+ // .end()
+ // .last()
+//  .css('background', 'aquamarine');
   
-  $('#meuElemento')
-  	.text('Treinamento')
-    .css({
-    	'color' : 'red',
-      'background': 'lightyellow'
-    })
+//	$('td').eq(4).css('background', 'aquamarine');
+  
+//  $('td').eq(4).add('b').css('background', 'aquamarine');
+  
+//	$('tr').not('.row2').css('background', 'aquamarine');
+
+//$('td').slice(2,7).css('background', 'aquamarine');
+
+//$('td').has('b').css('background', 'aquamarine');
+
+//$('td').eq(4).closest('tr').css('background', 'aquamarine');
+
+//$('td').eq(4).siblings().css('background', 'aquamarine');
+
+//$('td').eq(4).next().css('background', 'aquamarine');
+
+//$('td').eq(4).prev().css('background', 'aquamarine');
+
+//$('td').eq(4).nextAll().css('background', 'aquamarine');
+
+//$('td').eq(4).prevAll().css('background', 'aquamarine');
+
+//$('td').eq(4).nextUntil('.row3').css('background', 'aquamarine');
+
+//$('td').eq(4).prevUntil('.row3').css('background', 'aquamarine');
+
+//$('b').parent().css('background', 'aquamarine');
+
+//$('b').parents('table').css('background', 'aquamarine');
+
+//$('b').parentsUntil('table').css('background', 'aquamarine');
+
+//$('td').children().css('background', 'aquamarine');
+
+//$('td').contents().css('background', 'aquamarine');
+
+//$('.row2').find('td').css('background', 'aquamarine')
+
+	//$('div').css('background', 'lightblue');	
+  
+  //$('#meuElemento')
+  //	.text('Treinamento')
+  //  .css({
+  //  	'color' : 'red',
+  //    'background': 'lightyellow'
+  //  })
 })
