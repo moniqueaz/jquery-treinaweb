53820202ab6b602bc2809e0ae41c41e27a8012d0
Monique Azevedo <moniqueaz.si@gmail.com>
2017-12-02 10:48:27 -0200

    Introducao

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('div').css('background', 'lightblue');	
+  
+  $('#meuElemento')
+  	.text('Treinamento')
+    .css({
+    	'color' : 'red',
+      'background': 'lightyellow'
+    })
+})
